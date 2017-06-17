8b3a84a789046961071be7e42117bd1efb285067
humanSwissArmyKnife <quopperleaf@mail.com>
2017-06-16 21:12:15 -0700

    Linting & ignore

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
-from flask import Flask
-from redis import Redis, RedisError
+"""Sandbox App"""
 import os
 import socket
+from flask import Flask
+from redis import Redis, RedisError
 
 # Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+REDIS = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-@app.route("/")
+@APP.route("/")
 def hello():
+    """Default route output"""
     try:
-        visits = redis.incr("counter")
+        visits = REDIS.incr("counter")
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+    return html.format(name=os.getenv("NAME", "world"),
+                       hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80)
+    APP.run(host='0.0.0.0', port=80)
